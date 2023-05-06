d3c7f3448ab02a737f2215a9ee4753fe2a017ea2
Miles <miles@karmacheck.com>
2023-05-06 17:46:27 -0500

    day3 part 1finished

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
-}
+    let text = fs::read_to_string("input.txt").expect("Error reading");
+    
+    let mut score = 0;
+    for row in text.split("\n"){
+        println!("{}", row);
+        let split = row.split_at(row.len() / 2);
+        println!("{:#?}", split);
+        let first = split.0;
+        let second = split.1;
+        let s = first.chars().find(|&f| second.contains(f));
+        if s.is_none(){
+            println!("skipping");
+            continue;
+        }
+        let c = s.unwrap();
+        println!("{:#?}", c);
+        let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let point = alphabet.find(c).unwrap_or(0);
+        score += point + 1;
+    }
+
+    println!("{}", score);
+} 
